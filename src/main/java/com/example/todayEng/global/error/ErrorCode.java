60652d56d3ed50ba9f47c3b453b1431f817a584b
ownue <ownue.lee@gmail.com>
2026-07-27 17:57:05 +0900

    feat: 외부 계정 연동 관리 API 구현

--- src/main/java/com/example/todayEng/global/error/ErrorCode.java
+++ src/main/java/com/example/todayEng/global/error/ErrorCode.java
@@ -37,6 +37,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재하지 않는 사용자입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "U002", "이미 사용중인 이메일입니다."),
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "U003", "이미 사용중인 닉네임입니다."),
+    EXTERNAL_ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "U004", "연동된 외부 계정을 찾을 수 없습니다."),
 
 
     // External Service / S3 / API (E)
